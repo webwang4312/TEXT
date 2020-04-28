0c7a9c7b77217fc7ad212fc5f64b771c943f7a1a
webwang4312 <1873772924@qq.com>
2020-04-28 21:04:53 +0800

    加了一些

--- xcx/mp-demo/app.js
+++ xcx/mp-demo/app.js
@@ -6,17 +6,17 @@ const globalData = {
 
 App({
   onLaunch: function() {
-    console.log('生命周期回调：监听小程序初始化')
+    // console.log('生命周期回调：监听小程序初始化')
 
     wx.login({
       success: res => {
-        console.log('wx.login', res)
+        // console.log('wx.login', res)
       }
     })
     
     wx.getSetting({
       success: res => {
-        console.log('getSetting', res)
+        // console.log('getSetting', res)
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
--- xcx/mp-demo/pages/todos/todos.js
+++ xcx/mp-demo/pages/todos/todos.js
@@ -21,38 +21,6 @@ Page({
         status: 1
       }
     ],
-    handleTodo(event) {
-      console.log(event);
-      const { position } = event.detail;
-      switch(position) {
-        case 'left': {
-          console.log('左边打开了')
-          // 代办完成，将该代办从 todos 数组中删除，并且添加到 finishedTodos 中
-          let { index, id, name } = event.target.dataset;
-          let todos = this.data.todos
-          let finishedTodos = this.data.finishedTodos
-          todos.splice(index, 1)
-          finishedTodos.push({ id, name })
-          this.setData({
-            todos: todos,
-            finishedTodos: finishedTodos
-          })
-          break;
-        }
-        case 'right': {
-          console.log('右边打开了')
-          // 删除代办，将该代办从 todos 数组中删除
-          console.log(event)
-          let {index, id} = event.target.dataset;
-          let todos = this.data.todos
-          todos.splice(index, 1)
-          this.setData({
-            todos: todos
-          })
-          break;
-        }
-      }
-    },
     finishedTodos: [],
     unfinishedTodos: [],
     // 显示新增代办弹窗
@@ -155,7 +123,36 @@ Page({
       showEndDate: false
     })
   },
+  
   handleTodo(event) {
-    console.log(event)
+    const { position } = event.detail;
+    switch(position) {
+      case 'left': {
+        console.log('左边打开了');
+        // 代办完成，将该代办从 todos 数组中删除，并且添加到 finishedTodos 中
+        let {index} = event.target.dataset;
+        let todos = this.data.todos;
+         let finishedTodos = this.data.finishedTodos;
+        todos.splice(index, 1);
+        finishedTodos.push(index, 1);
+        this.setData({
+         finishedTodos: todos
+        });
+        break;
+      }
+      case 'right': {
+        console.log('右边打开了')
+        // 删除代办，将该代办从 todos 数组中删除
+        let {index} = event.target.dataset;
+        let todos = this.data.todos;
+         let unfinishedTodos = this.data.unfinishedTodos;
+        todos.splice(index, 1);
+        unfinishedTodos.push(index, 1);
+        this.setData({
+          unfinishedTodos: todos
+        });
+        break;
+      }
   }
+}
 })
